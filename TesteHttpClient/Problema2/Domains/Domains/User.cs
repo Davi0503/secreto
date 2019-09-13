7ff25ab3e430b38594a41425b94162286c54f89d
Davi Tavares Da Silva <44851410808@info.local>
2019-09-13 10:36:26 -0300

    colocando teste http

---
+++ TesteHttpClient/Problema2/Domains/Domains/User.cs
@@ -0,0 +1,12 @@
+﻿            using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domains.Domains
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ TesteHttpClient/Problema2/Services/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Services.Interfaces;
+
+namespace Services.Services
+{
+    public class UserService 
+    {
+        private static HttpClient hhtpClientInstance;
+
+        public static HttpClient GetHttpClientInstance()
+        {
+            if (hhtpClientInstance == null)
+            {
+                hhtpClientInstance = new HttpClient();
+                hhtpClientInstance.DefaultRequestHeaders.ConnectionClose = false;
+            }
+            return hhtpClientInstance;
+        }
+        public string baseUrl
+        {
+            get
+            {
+                return "https://pokeapi.co/api/v2/";
+            }
+        }
+
+
+
+        public string getPokemon()
+        {
+            string action = "pokemon/ditto/";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, baseUrl + action);
+            HttpResponseMessage response = GetHttpClientInstance().SendAsync(request).Result;
+        }
+    }
+}
